b5abd8b3c1fd84c396df8d9faf008c128485b335
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-01-09 15:13:56 +0100

    hej

--- code/jacobi.c
+++ code/jacobi.c
@@ -4,6 +4,19 @@
 #include <math.h>
 
 void
-jacobi(int jacobi(double ***, double ***, double ***, int, int, double *)) {
-    // fill in your code here
+jacobi(double ***, double ***, double ***, int N, int nmax, double *) {
+    
+
+    for (int n = 0; n <; n++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+
+                }
+            }
+        }
+    }
+
+
+
 }
