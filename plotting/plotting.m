0a606809b68c42e7d7aa145f56a6f9692b33dc60
s194146 <nikoline@rehn.dk>
2024-01-10 09:57:01 +0100

    matlab

--- plotting/plotting.m
+++ plotting/plotting.m
@@ -1,2 +1,45 @@
 
+%% Plot results
+
 clear; close all; clc;
+
+N = 5;
+
+file = "../code/results/output.txt";
+
+matrix = readmatrix(file);
+
+data = cell(N+2,1);
+
+for i=1:(N+2)
+    data{i} = matrix(((i-1)*(N+2)+1):(i*(N+2)),:);
+end
+
+
+X = linspace(-1,1,N+2);
+
+figure;
+subplot(1,3,1)
+imagesc(X,X,data{2})
+axis equal
+axis tight
+title(sprintf('$i = %d$',1),'Interpreter','latex','FontSize',16)
+ylabel('$y$','Interpreter','latex','FontSize',16)
+xlabel('$z$','Interpreter','latex','FontSize',16)
+subplot(1,3,2)
+imagesc(X,X,data{round((N+1)/2)})
+axis equal
+axis tight
+title(sprintf('$i = %d$',round((N+1)/2)),'Interpreter','latex','FontSize',16)
+ylabel('$y$','Interpreter','latex','FontSize',16)
+xlabel('$z$','Interpreter','latex','FontSize',16)
+subplot(1,3,3)
+imagesc(X,X,data{N+1})
+axis equal
+axis tight
+title(sprintf('$i = %d$',N),'Interpreter','latex','FontSize',16)
+ylabel('$y$','Interpreter','latex','FontSize',16)
+xlabel('$z$','Interpreter','latex','FontSize',16)
+
+
+exportgraphics(gcf,sprintf('solution_%d.png',N),'Resolution',300);
